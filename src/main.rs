b6a79b5aa5637ccf22d1252ab19cfd6fb1d66a76
Max G <maxdotdotg@gmail.com>
2020-01-18 23:09:44 -0500

    add Rectangle struct with debug trait and area function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 // example program using structs
 // ch05-02-example-structs
 
-fn main() {
-    let width1 = 30;
-    let height1 = 50;
-
-    println!("the are of the rectangle is {}", area(width1, height1));
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn main() {
+    // first, variables are separate
+    //let width1 = 30;
+    //let height1 = 50;
+
+    // refactored to be elements of one variable
+    // let rect1 = (30, 50);
+
+    // refactored to be an instance of Rectangle
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("the are of the rectangle is {}", area(&rect1));
+    println!("{:?}", rect1);
+
+}
+
+// refactored to take a reference to Rectangle as input
+// previously took dimensions as ints or as a tuple
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
